b9f33e4bce552cab25e27a1392c8ac0097cea654
fabianandresp <pimiento.fabian@gmail.com>
2021-02-20 22:07:04 -0600

    actualizacion

--- src/com/company/Padecimiento.java
+++ src/com/company/Padecimiento.java
@@ -5,27 +5,20 @@ import java.util.ArrayList;
 public class Padecimiento {
     private String nombrePadecimiento;
     private String tipoPadecimiento;
-    ArrayList<String> tipo = new ArrayList<>();
-    ArrayList<String> descripcion = new ArrayList<>();
+    ArrayList<Padecimiento> tipo = new ArrayList<>();
 
     public Padecimiento(String nombrePadecimiento, String tipoPadecimiento) {
         this.nombrePadecimiento = nombrePadecimiento;
         this.tipoPadecimiento = tipoPadecimiento;
     }
 
-    public String getNombrePadecimiento() {
-        return nombrePadecimiento;
-    }
+    public void registroPadecimientos() {
+        tipo.add(new Padecimiento("Electrocardiograma", " Cardiologia"));
+        tipo.add(new Padecimiento(" ", " "));
+        tipo.add(new Padecimiento(" ", " "));
+        tipo.add(new Padecimiento(" ", " "));
+        tipo.add(new Padecimiento(" ", " "));
 
-    public void setNombrePadecimiento(String nombrePadecimiento) {
-        this.nombrePadecimiento = nombrePadecimiento;
-    }
 
-    public String getTipoPadecimiento() {
-        return tipoPadecimiento;
-    }
-
-    public void setTipoPadecimiento(String tipoPadecimiento) {
-        this.tipoPadecimiento = tipoPadecimiento;
     }
 }
