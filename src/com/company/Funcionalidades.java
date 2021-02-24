79e72099557be051e8895343da4691416e72d324
Fabian Pimiento <71538925+fabianandresp@users.noreply.github.com>
2021-02-23 22:33:40 -0600

    Modificacion de clase de funcionalidades

--- src/com/company/Funcionalidades.java
+++ src/com/company/Funcionalidades.java
@@ -2,24 +2,32 @@ package com.company;
 
 // En esta clase hay metodos que tienen funciones que ayudan a en funciones de todo el sistema
 
+import javax.swing.*;
 import java.util.Scanner;
 
 public class Funcionalidades {
     Scanner input = new Scanner(System.in);
-    int opcion;
+    String opcion;
     String mensaje_a_medico;
+    int califi;
 
     public void auto_email() {
-        System.out.println("Desea enviar un correo al Medico?");
-        if (opcion == 1) {
-            System.out.println("Digite su correo");
-            mensaje_a_medico = input.nextLine();
-            calificacion();
-        } else {
-            System.out.println("No se ha enviado ningun mensaje al Medico");
-            calificacion();
-        }
+        while(true) {
+            System.out.println("Desea enviar un correo al Medico?");
+            opcion = input.nextLine();
+            if (opcion.equals("si")) {
+                System.out.println("Digite su correo");
+                mensaje_a_medico = input.nextLine();
+                calificacion();
 
+            } else if (opcion.equals("no")) {
+                System.out.println("No se ha enviado ningun mensaje al Medico");
+                calificacion();
+            } else {
+                JOptionPane.showMessageDialog(null, "Error, por favor escriba si o no");
+
+            }
+        }
     }
 
     //Metodo para calificar al medico (Esto se puede hacer por la interfaz)
@@ -30,9 +38,24 @@ public class Funcionalidades {
         calificacion[2] = 3;
         calificacion[3] = 4;
         calificacion[4] = 5;
+        System.out.println(" ____________________________________________");
         System.out.println("Agradeceriamos nos brindara su calificacion");
+
         for (int i = 0; i < calificacion.length; i++)
-            System.out.println("1.- " + calificacion[i] + "estrellas");
+            System.out.println( calificacion[i] + " Estrellas");
+        califi = input.nextInt();
+        switch (califi){
+            case 1: JOptionPane.showMessageDialog(null,"Se enviado una calificacion de 1 estrella al medico");
+            break;
+            case 2: JOptionPane.showMessageDialog(null,"Se enviado una calificacion de 2 estrella al medico");
+            break;
+            case 3: JOptionPane.showMessageDialog(null,"Se enviado una calificacion de 3 estrella al medico");
+            break;
+            case 4: JOptionPane.showMessageDialog(null,"Se enviado una calificacion de 4 estrella al medico");
+            break;
+            case 5: JOptionPane.showMessageDialog(null,"Se enviado una calificacion de 5 estrella al medico");
+            break;
+        }
 
 
     }
